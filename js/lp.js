c58d013d405e463de1b2348367fe5745007da070
liquidz <liquidz.uo@gmail.com>
2009-02-05 14:47:42 +0900

    only use 'lp'

--- js/lp.js
+++ js/lp.js
@@ -1,24 +1,49 @@
-/* global variables */
-var CURRENT_PAGE = 0;
-var SLIDE_LENGTH = null;
-var OPTIONS = {};
+/* ================================================
 
-/* common func */
-function elem(tag){
-	return jQuery(document.createElement(tag));	
-}
-function p(text){
-	return elem("p").text(text);
-}
+	Lazy Presentation v0.04
 
-/* =Slide Class */
-function Slide(title, page){
+	Copyright (C) 2009 Masashi Iizuka
+	Dual licensed under the MIT and GPL licenses
+
+	Last update: 2009-02-05
+
+   ================================================ */
+
+if(typeof window.lp != 'undefined') delete window.lp;
+
+var lp = {};
+lp.slide = [];
+lp.current_slide = 0;
+lp.options = {};
+
+lp.common = {
+	elem: function(tag){
+		return jQuery(document.createElement(tag));
+	},
+
+	p: function(text){
+		return this.elem("p").text(text);
+	}
+};
+
+lp.class = {
+	slide: ["slide", "main"],
+	pager: "pager",
+	sublist: "sublist"
+};
+lp.id = {
+	slide: "#slide"
+};
+
+/* =Slide Class
+ ---------------------------------------------- */
+lp.Slide = function(title, page){
 	this.title = title;
 	this.page = page;
 	this.body = new Array();
 	this.subsection = false;
 };
-Slide.prototype = {
+lp.Slide.prototype = {
 	add: function(obj){
 		this.body.push(obj);
 		return this;
@@ -29,30 +54,31 @@ Slide.prototype = {
 	},
 	
 	to_s: function(){
-		var box = elem("div");
-		var main = elem("div");
-		box.attr("id", "slide" + this.page);
-		box.addClass("slide");
-		main.addClass("main");
-		main.append(elem("h2").text(this.title));
+		var box = lp.common.elem("div");
+		var main = lp.common.elem("div");
+		box.attr("id", lp.id.slide.substr(1) + this.page);
+		box.addClass(lp.class.slide[0]);
+		main.addClass(lp.class.slide[1]);
+		main.append(lp.common.elem("h2").text(this.title));
 
 		jQuery.each(this.body, function(){
 			main.append(this);
 		});
 
 		box.append(main);
-		box.append(p(this.page).addClass("pager"));
+		box.append(lp.common.p(this.page).addClass(lp.class.pager));
 		return box;
 	}
 };
 
-/* =List Class */
-function List(){
+/* =List Class
+ ---------------------------------------------- */
+lp.List = function(){
 	this.active_flag = false;
 	this.body = null;
 	this.is_sub_list = false;
 };
-List.prototype = {
+lp.List.prototype = {
 	sub_list: function(bool){
 		this.is_sub_list = bool;
 	},
@@ -62,13 +88,13 @@ List.prototype = {
 
 			var tmp = this.body;
 			this.body = null;
-			return (this.is_sub_list) ? tmp.addClass("sublist") : tmp;
+			return (this.is_sub_list) ? tmp.addClass(lp.class.sublist) : tmp;
 		}
 		return null;
 	},
 	add: function(obj){
 		if(!this.active_flag){
-			this.body = elem("ul");
+			this.body = lp.common.elem("ul");
 			this.active_flag = true;
 		}
 		this.body.append(obj);
@@ -78,16 +104,17 @@ List.prototype = {
 	}
 };
 
-/* =parse_contents */
-function parse_contents(cont){
-	var list = new List();
+/* =parse_contents
+ ---------------------------------------------- */
+lp.parse_contents = function(cont){
+	var list = new lp.List();
 	var page = null;
 	var result = new Array();
 	var page_count = 1;
 
 	var lists = {};
 	var last_level = 0;
-	lists[0] = new List();
+	lists[0] = new lp.List();
 
 	var str = cont;
 	while(str != ""){
@@ -108,7 +135,7 @@ function parse_contents(cont){
 			var level = RegExp.$1.length;
 			var title = RegExp.$2;
 			var rest = RegExp.rightContext
-			page = new Slide(title, page_count++);
+			page = new lp.Slide(title, page_count++);
 			if(level > 1) page.set_subsection(true);
 			str = jQuery.trim(rest);
 		} else if(str.match(/^(\*+)\s*(.+?)(\n|$)/)){
@@ -116,10 +143,9 @@ function parse_contents(cont){
 			var level = RegExp.$1.length - 1;
 			var text = RegExp.$2;
 			var rest = RegExp.rightContext;
-			//document.write("kiteru?"+level+"<br>");
 
 			if(level > last_level){
-				lists[level] = new List();
+				lists[level] = new lp.List();
 				if(level > 0) lists[level].sub_list(true);
 			} else if(level < last_level){
 				for(var i = last_level; i > level; --i){
@@ -127,10 +153,8 @@ function parse_contents(cont){
 					lists[i] = null;
 				}
 			}
-			lists[level].add(elem("li").text(text));
-
+			lists[level].add(lp.common.elem("li").text(text));
 			last_level = level;
-
 			str = jQuery.trim(rest);
 
 			// $
@@ -156,7 +180,7 @@ function parse_contents(cont){
 
 			var text = RegExp.$1;
 			var rest = RegExp.rightContext;
-			page.add(elem("div").addClass("code").append(elem("code").addClass("prettyprint").text(jQuery.trim(text))));
+			page.add(lp.common.elem("div").addClass("code").append(lp.common.elem("code").addClass("prettyprint").text(jQuery.trim(text))));
 			str = jQuery.trim(rest);
 		} else if(str.match(/^\%(.+?)(\n|$)/)){
 			// option
@@ -164,7 +188,7 @@ function parse_contents(cont){
 			var rest = RegExp.rightContext;
 
 			var val = text.split("=");
-			if(val.length == 2) OPTIONS[jQuery.trim(val[0])] = jQuery.trim(val[1]);
+			if(val.length == 2) lp.options[jQuery.trim(val[0])] = jQuery.trim(val[1]);
 			str = jQuery.trim(rest);
 		} else {
 			// p
@@ -182,10 +206,10 @@ function parse_contents(cont){
 			if(str.match(/(.+?)\n/)){
 				var text = RegExp.$1;
 				var rest = RegExp.rightContext;
-				page.add(p(text));
+				page.add(lp.common.p(text));
 				str = jQuery.trim(rest);
 			} else {
-				page.add(p(str));
+				page.add(lp.common.p(str));
 				str = "";
 			}
 		}
@@ -193,91 +217,100 @@ function parse_contents(cont){
 	result.push(page);
 
 	return result;
-}
+};
 
-/* =toggle_slide */
-function toggle_slide(from, to){
-	$("#slide" + from).hide(500);
-	$("#slide" + to).show(500);
-}
+/* =toggle_slide
+ ---------------------------------------------- */
+lp.toggle_slide = function(from, to){
+	$(lp.id.slide + from).hide(500);
+	$(lp.id.slide + to).show(500);
+};
 
-/* =next */
-function next(){
-	var last = CURRENT_PAGE;
-	if(CURRENT_PAGE < SLIDE_LENGTH) ++CURRENT_PAGE;
-	else CURRENT_PAGE = 0;
-	toggle_slide(last, CURRENT_PAGE);
-}
-/* =prev */
-function prev(){
-	var last = CURRENT_PAGE;
-	if(CURRENT_PAGE > 0) --CURRENT_PAGE;
-	else CURRENT_PAGE = SLIDE_LENGTH;
-	toggle_slide(last, CURRENT_PAGE);
-}
+/* =next
+ ---------------------------------------------- */
+lp.next = function(){
+	var last = lp.current_slide;
+	if(lp.current_slide < lp.slide.length) ++lp.current_slide;
+	else lp.current_slide = 0;
+	lp.toggle_slide(last, lp.current_slide);
+};
+/* =prev
+ ---------------------------------------------- */
+lp.prev = function(){
+	var last = lp.current_slide;
+	if(lp.current_slide > 0) --lp.current_slide;
+	else lp.current_slide = lp.slide.length;
+	lp.toggle_slide(last, lp.current_slide);
+};
 
-/* =move_page */
-function move_page(e){
+/* =move_page
+ ---------------------------------------------- */
+lp.move_page = function(e){
 	var mx = e.pageX;
-	var w = $("#slide0").width();
+	var w = $(lp.id.slide + "0").width();
 
-	(mx < w/2) ? prev() : next();
-}
+	(mx < w/2) ? lp.prev() : lp.next();
+};
 
-/* =update_font_size */
-function update_font_size(){
-	var toc = $("#slide0");
-	var title = $("#slide0 div.main h2")
+/* =update_font_size
+ ---------------------------------------------- */
+lp.update_font_size = function(){
+	var toc = $(lp.id.slide + "0");
+	var title = $(lp.id.slide + "0 div." + lp.class.slide[1] + " h2")
 	var font_size = (toc.width() * 3 / 5) / ((title.text().length > 10) ? title.text().length / 2 : title.text().length);
 	font_size = (font_size > toc.height() / 12) ? toc.height() / 12 : font_size;
 	$("h2").css("font-size", font_size + "px");
 	$("body").css("font-size", font_size * 2 / 5 + "px");
 	$("code").css("font-size", font_size / 3 + "px");
-	$("p.pager").css("font-size", font_size / 3 + "px");
-}
+	$("p." + lp.class.pager).css("font-size", font_size / 3 + "px");
+};
 
-/* =table_of_contents */
-function table_of_contents(title, slides){
-	var toc = new Slide(title, 0);
-	var list = new List();
+/* =table_of_contents
+ ---------------------------------------------- */
+lp.table_of_contents = function(){
+	var title = lp.slide[0].title;
+	var toc = new lp.Slide(title, 0);
+	var list = new lp.List();
 
 	// date
 	var date = "";
-	if(OPTIONS["date"]) date = OPTIONS["date"];
+	if(lp.options["date"]) date = lp.options["date"];
 	// author
 	var author = "";
-	if(OPTIONS["author"]) author = OPTIONS["author"];
+	if(lp.options["author"]) author = lp.options["author"];
 	var tmp = jQuery.trim(date + " " + author);
-	if(tmp != "") toc.add(p(tmp).addClass("center"));
+	if(tmp != "") toc.add(lp.common.p(tmp).addClass("center"));
 
 	// sections
-	toc.add(p("Table of Contents"));
-	jQuery.each(slides, function(){
+	toc.add(lp.common.elem("h3").text("Table of Contents"));
+	jQuery.each(lp.slide, function(){
 		if(!this.subsection)
-			list.add(elem("li").text(this.title));
+			list.add(lp.common.elem("li").text(this.title));
 	});
 	toc.add(list.finish());
 
 	return toc.to_s();
-}
+};
 
-/* =initialize */
-function initialize(){
+/* =initialize
+ ---------------------------------------------- */
+lp.initialize = function(){
 	// change font size
-	update_font_size();
+	lp.update_font_size();
 
 	// add events
 	var w = $(window);
-	w.bind("resize", update_font_size);
-	w.bind("click", move_page);
+	w.bind("resize", lp.update_font_size);
+	w.bind("click", lp.move_page);
 
 	// set options
 	var body = $("body");
-	if(OPTIONS["bg"]) body.css("background", OPTIONS["bg"]);
-	if(OPTIONS["fg"]) body.css("color", OPTIONS["fg"]);
-}
+	if(lp.options["bg"]) body.css("background", lp.options["bg"]);
+	if(lp.options["fg"]) body.css("color", lp.options["fg"]);
+};
 
-/* =main */
+/* =main
+ ---------------------------------------------- */
 $(function(){
 	var title = $("head title").text();
 	var body = $("body");
@@ -287,20 +320,19 @@ $(function(){
 	body.text("");
 
 	/* parse body text */
-	var slides = parse_contents(contents);
-	SLIDE_LENGTH = slides.length;
+	lp.slide = lp.parse_contents(contents);
 
 	/* add table of contents(page = 0) */
-	body.append(table_of_contents(title, slides));
+	body.append(lp.table_of_contents());
 
-	/* add each slides(page = 1 - SLIDE_LENGTH)*/
-	jQuery.each(slides, function(){
+	/* add each slides(page = 1 - lp.slide.length)*/
+	jQuery.each(lp.slide, function(){
 		body.append(this.to_s());
-		$("#slide" + this.page).hide();
+		$(lp.id.slide + this.page).hide();
 	});
 
 	/* initialize */
-	initialize();
+	lp.initialize();
 	
 	prettyPrint();
 });
